1eed1edc70d614a6f05f5e50d751339ecd761564
Mormacill <31103546+Mormacill@users.noreply.github.com>
2020-11-22 23:34:05 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,9 @@
 #start ssh
 service ssh start 
 
-#start jobmanager openPBS
+#openPBS
+sed -i s/PBS_SERVER=.*/PBS_SERVER=$HOSTNAME/g /etc/pbs.conf
+sed -i s/'$clienthost.*'/'$clienthost'\ $HOSTNAME/g /var/spool/pbs/mom_priv/config
 service pbs start
 
 #start bash
