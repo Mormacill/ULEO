b1d5ec242f29bd58cc1ddb1768281c3e4dadd40b
Mormacill <31103546+Mormacill@users.noreply.github.com>
2020-12-02 14:51:19 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,5 +8,11 @@ sed -i s/PBS_SERVER=.*/PBS_SERVER=$HOSTNAME/g /etc/pbs.conf
 sed -i s/'$clienthost.*'/'$clienthost'\ $HOSTNAME/g /var/spool/pbs/mom_priv/config
 service pbs start
 
+#xfce4 and xrdp
+rm /var/run/xrdp/xrdp.pid & sleep 2
+service dbus start
+service xrdp start
+
+
 #start bash
 /bin/bash
