da2c763697f6d65785e0417db16a80db8ee18160
Mormacill <31103546+Mormacill@users.noreply.github.com>
2021-02-03 21:08:26 +0100

    revised full healthcheck script

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-#Lmod
-module --help || exit 1
+###define test functions
+function TestLmod {
+  echo "test lmod" && source /usr/sbin/lmod/$LMODVERSION/init/bash && module --help &> /dev/null
+  if [ $? -ne 0 ]; then
+          exit 1
+  fi
+}
+function TestEasyBuild {
+  echo "test build" && export MODULEPATH=$MODULEPATH:/home/easybuilder/.local/EasyBuildInst/modules/all && module add EasyBuild && eb --help &> /dev/null
+  if [ $? -ne 0 ]; then
+          exit 1
+  fi
+}
+function TestOpenPBS {
+  echo "test pbs" && source /etc/profile.d/pbs.sh && qstat -a &> /dev/null
+  if [ $? -ne 0 ]; then
+          exit 1
+  fi
+}
+function TestXRDP {
+  echo "test xrdp" && service xrdp status &> /dev/null
+  if [ $? -ne 0 ]; then
+          exit 1
+  fi
+}
 
-#EasyBuild
-export MODULEPATH=$MODULEPATH:/home/easybuilder/.local/EasyBuildInst/modules/all && module add EasyBuild && eb --help || exit 1
+function Testsamba {
+  echo "test samba" && service smbd status &> /dev/null
+  if [ $? -ne 0 ]; then
+          exit 1
+  fi
+}
 
-#OpenPBS
-qstat -a || exit 1
-
-#XRDP
-service xrdp status || exit 1
+###start tests
+TestLmod
+TestEasyBuild
+TestOpenPBS
+TestXRDP
+Testsamba
