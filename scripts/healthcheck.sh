66c45e138bcf319806be3c6af9a813c3a2037ef0
Mormacill <31103546+Mormacill@users.noreply.github.com>
2021-02-03 13:49:21 +0100

    Create healthcheck.sh

---
+++ scripts/healthcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Lmod
+module --help || exit 1
+
+EasyBuild
+export MODULEPATH=$MODULEPATH:/home/easybuilder/.local/EasyBuildInst/modules/all && module add EasyBuild && eb --help || exit 1
+
+#OpenPBS
+qstat -a || exit 1
+
+#XRDP
+service xrdp status || exit 1
