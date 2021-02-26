f5a187b85ee814b0c35ab0fea310c266a5e09f83
Mormacill <31103546+Mormacill@users.noreply.github.com>
2021-02-26 11:56:36 +0100

    added some comments

--- scripts/samba/smb_useradd.sh
+++ scripts/samba/smb_useradd.sh
@@ -8,11 +8,11 @@ EOF
 exit 1
 fi
 
-  USERN=$1                #Name of user to add
+  USERN=$1                                  #Name of user to add
 
-sudo adduser --force-badname $USERN
-sudo smbpasswd -a $USERN
-sudo mkdir /scratch/$USERN
-sudo chown $USERN:$USERN /scratch/$USERN
-sudo chmod 700 /scratch/$USERN
-sudo usermod -aG smbusers $USERN
+sudo adduser --force-badname $USERN         #add user to system
+sudo smbpasswd -a $USERN                    #add user to samba
+sudo mkdir /scratch/$USERN                  #create user scratch directory
+sudo chown $USERN:$USERN /scratch/$USERN    #change owner of user scratch directory
+sudo chmod 700 /scratch/$USERN              #change rights of user scratch directory
+sudo usermod -aG smbusers $USERN            #add user to group smbusers to grant user authentication for samba
