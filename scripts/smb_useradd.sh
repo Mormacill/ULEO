9d62d4c9943ccda78e9541cbc159aaedb680c76e
Mormacill <31103546+Mormacill@users.noreply.github.com>
2021-02-26 11:49:36 +0100

    Create smb_useradd.sh
    script for smb user add automation

---
+++ scripts/smb_useradd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -lt 1 ] || [ $# -gt 1 ]; then
+cat <<EOF
+wrong count of arguments
+- usage: $0 USERNAME
+EOF
+exit 1
+fi
+
+  USERN=$1                #Name of user to add
+
+sudo adduser $USERN
+sudo smbpasswd -a $USERN
+sudo mkdir /scratch/$USERN
+sudo chown $USERN:$USERN /scratch/$USERN
+sudo chmod 700 /scratch/$USERN
+sudo usermod -aG smbusers $USERN
